93af89acad033feac129a22fb1b5e9cc41cb6bc1
HevalNergiz <1hevalnergiz1@gmail.com>
2022-03-06 16:49:34 +0300

    abooww

---
+++ assets/js/draggable.js
@@ -0,0 +1,50 @@
+// https://www.html5rocks.com/de/tutorials/dnd/basics/#toc-references
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+   dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) { e.preventDefault(); }
+  e.dataTransfer.dropEffect = 'move';  
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) { e.stopPropagation(); }
+
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  [].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });
+}
+
+var cols = $('[data-draggable]').children();
+[].forEach.call(cols, function(col) {
+  col.addEventListener('dragstart', handleDragStart, false);
+  col.addEventListener('dragenter', handleDragEnter, false)
+  col.addEventListener('dragover', handleDragOver, false);
+  col.addEventListener('dragleave', handleDragLeave, false);
+  col.addEventListener('drop', handleDrop, false);
+  col.addEventListener('dragend', handleDragEnd, false);
+});
